430f1c3fdc584f8049d112fb311dbcee4809be30
HidalgoIvan <A01651425@itesm.mx>
2019-09-08 23:28:56 -0500

    basic game functionality

--- ecard.js
+++ ecard.js
@@ -1,112 +1,172 @@
-﻿var winningScore = 10;
-var startingComputerHand = ["S","C","C"];
-var startingHumanHand = ["K","C","C"];
+﻿var winningScore = 10000;
+var totalWinnings = 0;
+var startingComputerHand = ["S","C","C","C","C"];
+var startingHumanHand =    ["K","C","C","C","C"];
 var finalChoiceNodes = {};
+var computerRole = "Slave";
 var canPlay = true;
+var computerCards = [];
+var viableComputerCards = [0,1,2,3,4];
 document.addEventListener('DOMContentLoaded', function () {   
-    var nodoLoco = new Node(startingComputerHand, startingHumanHand, null, "Human");
-    //printer(emperorMinMax(nodoLoco, true));
-    printer(slaveMinMax(nodoLoco, true));
-    console.log(finalChoiceNodes);
-    function emperorMinMax(node, maximizingPlayer){
-        if(checkTerminalNodeEmperor(node)){
-            // Heuristic values
-            if(!node.computerHand.includes("K") && node.humanHand.includes("S"))
-            {
-                return winningScore;
-            }
-            if(node.computerHand.includes("K") && node.humanHand.includes("S"))
-            {
-                return winningScore * -5;
-            }
-            if(node.computerHand.includes("K") && !node.humanHand.includes("S"))
-            {
-                return winningScore;
-            }
-            if(node.choice.includes("S") && !node.computerHand.includes("K"))
-            {
-                return winningScore * -5;
-            }
-            if(node.choice.includes("K") && !node.humanHand.includes("S"))
-            {
-                return winningScore;
-            }
-            if(node.choice == "C" && !node.computerHand.includes("K"))
-            {
-                return winningScore;
-            }
-            if(node.choice == "C" && !node.humanHand.includes("S"))
-            {
-                return winningScore;
-            }
-            else{
-                return 0;
-            }
-        }
-        if(maximizingPlayer){
-            var value = -10000;
-            var result = 0;
-            node.children.forEach(child => {
-                result = emperorMinMax(child, false);
-                if(node.computerHand.length == startingComputerHand.length)
-                {
-                    finalChoiceNodes[child.choice] = result;
-                }
-                if(result > value)
-                {
-                    value = result;
-                }
-            });
-            return value;
-        }else{
-            var result = 0;
-            node.children.forEach(child => {
-                var prob = child.probability * emperorMinMax(child, true); 
-                result += prob; 
-            });
-            return result;
-        }
-    }
-
-    function frequency(arr, val)
-    {
-        var freq = 0;
-        arr.forEach(el => {
-            if(el.includes(val))
-            {
-                freq++;
-            }
-        });
-        return freq;
-    }
-    function printer(msg){
-        document.getElementsByClassName("printer")[0].innerText = msg;
-    }
+    computerCards = document.getElementsByClassName("cardback");
 });
 
-function playTurn(element){
-    if(!canPlay)
+async function playTurn(element){
+    if(!canPlay) // The animations are busy playing
     {
         return;
     }
-    var computerCards = document.getElementsByClassName("cardback");
-    var selectedComputerCard = computerCards[getRandomInt(computerCards.length)];
-    var selectedCardFront = document.createElement("div");
-    selectedCardFront.classList.add("card");
-    selectedCardFront.style.minWidth = "100%";
-    selectedCardFront.style.minHeight = "100%";
-    selectedCardFront.id = "emperor";
-    selectedCardFront.style.transform= "translateZ(-5px)";
-    selectedCardFront.style.opacity = "0";
-    selectedComputerCard.appendChild(selectedCardFront);
+    canPlay = false;
+    let playerChoice = element.id;
+    let computerChoice = "";
+    winningScore = parseInt(document.getElementById("bet-amount").value.replace(',',''));
+    finalChoiceNodes = {};
+    if(computerRole == "Slave")
+    {
+        var startingNode = new Node(startingComputerHand, startingHumanHand, null, "Human", 0, null);
+        slaveMinMax(startingNode, true);
+        if(!finalChoiceNodes.hasOwnProperty("C"))
+        {
+            computerChoice = "slave";
+        }else
+        // The path difference is significant, otherwise it will just get lost by decimals and the real choice is arbitrary
+        if(Math.abs(finalChoiceNodes["C"] - finalChoiceNodes["S"]) > .01)
+        {
+            if(finalChoiceNodes["C"] > finalChoiceNodes["S"])
+            {
+                computerChoice = "citizen";
+            }else{
+                computerChoice = "slave";
+            }
+        }else{
+            if(getRandomInt(2) == 0)
+            {
+                computerChoice = "citizen";
+            }else{
+                computerChoice = "slave";
+            }
+        }
+    }else{ // Emperor role
+        var startingNode = new Node(startingComputerHand, startingHumanHand, null, "Human", 0);
+        emperorMinMax(startingNode, true);
+        if(!finalChoiceNodes.hasOwnProperty("C"))
+        {
+            computerChoice = "emperor";
+        }else
+        if(Math.abs(finalChoiceNodes["C"] - finalChoiceNodes["K"]) > .01)
+        {
+            if(finalChoiceNodes["C"] > finalChoiceNodes["K"])
+            {
+                computerChoice = "citizen";
+            }else{
+                computerChoice = "emperor";
+            }
+        }else{
+            if(getRandomInt(2) == 0)
+            {
+                computerChoice = "citizen";
+            }else{
+                computerChoice = "emperor";
+            }
+        }
+    }
+    var chosenComputerCardIndex = getRandomInt(viableComputerCards.length);
+    var selectedComputerCard = computerCards[viableComputerCards[chosenComputerCardIndex]];
+    flipCards(element, selectedComputerCard , computerChoice);
+    viableComputerCards.splice(viableComputerCards.indexOf(viableComputerCards[chosenComputerCardIndex]),1);
+    await sleep(2000);
+    if(playerChoice == "emperor"){
+        startingHumanHand.splice(startingHumanHand.indexOf("K"), 1);
+    }
+    if(playerChoice == "citizen"){
+        startingHumanHand.splice(startingHumanHand.indexOf("C"), 1);
+    }
+    if(playerChoice == "slave"){
+        startingHumanHand.splice(startingHumanHand.indexOf("S"), 1);
+    }
+    if(computerChoice == "emperor"){
+        startingComputerHand.splice(startingComputerHand.indexOf("K"), 1);
+    }
+    if(computerChoice == "citizen"){
+        startingComputerHand.splice(startingComputerHand.indexOf("C"), 1);
+    }
+    if(computerChoice == "slave"){
+        startingComputerHand.splice(startingComputerHand.indexOf("S"), 1);
+    }
+    let startRound = false;
+    if(playerChoice != "citizen" || computerChoice != "citizen") // Winning or losing round
+    {
+        if((playerChoice == "citizen" && computerChoice == "emperor") || (playerChoice == "slave" && computerChoice == "citizen")) // User loses vs emperor or vs citizen
+        {
+            document.getElementById("lose").style.display = "inherit";
+            totalWinnings += -winningScore;
+        }
+        if(playerChoice == "emperor" && computerChoice == "slave") // User loses vs slave
+        {
+            document.getElementById("lose").style.display = "inherit";
+            totalWinnings = totalWinnings - (5 * winningScore);
+        }
+        if(playerChoice == "slave" && computerChoice == "emperor") // User wins vs emperor
+        {
+            document.getElementById("win").style.display = "inherit";
+            totalWinnings = totalWinnings + (5 * winningScore);
+        }
+        if(playerChoice == "emperor" && computerChoice == "citizen" || playerChoice == "citizen" && computerChoice == "slave")
+        {
+            document.getElementById("win").style.display = "inherit";
+            totalWinnings += winningScore;
+        }
+        await sleep(2000);
+        document.getElementById("lose").style.display = "none";
+        document.getElementById("win").style.display = "none";
+        startRound = true;
+    }
+    document.getElementById("score-value").innerText = (totalWinnings + "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    element.style.minWidth = "190px";
+    element.style.minHeight = "250px";
+    selectedComputerCard.style.animation = "none";
+    element.style.display = "none";
+    selectedComputerCard.style.display = "none";
+    if(startRound)
+    {
+        startNextRound();
+    }
+    canPlay = true;
+    // Uncomment this to see the considered paths!
+    //console.log(finalChoiceNodes);
+}
+function startNextRound(){
+    let playerCards = document.getElementsByClassName("card");
+    for(var x = 0; x < computerCards.length; x++)
+    {
+        playerCards[x].style.display = "inherit";
+        computerCards[x].style.display = "inherit";
+    }
+    viableComputerCards = [0,1,2,3,4];
+    if(computerRole == "Slave") // Computer WAS playing as slave
+    {
+        computerRole = "Emperor";
+        document.getElementById("emperor").id = "slave";
+        startingComputerHand = ["K","C","C","C","C"];
+        startingHumanHand = ["S","C","C","C","C"];
+    }else{ // Computer WAS playing as emperor
+        computerRole = "Slave";
+        document.getElementById("slave").id = "emperor";
+        startingComputerHand = ["S","C","C","C","C"];
+        startingHumanHand = ["K","C","C","C","C"];
+    }
+}
+function flipCards(element, selectedComputerCard, choice){
     element.style.minWidth = "220px";
     element.style.minHeight = "290px";
     element.style.boxShadow = "0px 10px 10px -6px rgba(0,0,0,0.75)";
-    selectedComputerCard.style.animation = "reveal-card 2s forwards"; 
-    //moveElementToX(element, 50);
+    selectedComputerCard.style.animation = "reveal-card-" + choice + " 2s forwards"; 
+}
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 function getRandomInt(max) {
-return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 function slaveMinMax(node, maximizingPlayer){
     if(checkTerminalNodeSlave(node)){
@@ -168,20 +228,65 @@ function slaveMinMax(node, maximizingPlayer){
     }
 }
 
+function emperorMinMax(node, maximizingPlayer){
+    if(checkTerminalNodeEmperor(node)){
+        // Heuristic values
+        if(!node.computerHand.includes("K") && node.humanHand.includes("S"))
+        {
+            return winningScore;
+        }
+        if(node.computerHand.includes("K") && node.humanHand.includes("S"))
+        {
+            return winningScore * -5;
+        }
+        if(node.computerHand.includes("K") && !node.humanHand.includes("S"))
+        {
+            return winningScore;
+        }
+        if(node.choice.includes("S") && !node.computerHand.includes("K"))
+        {
+            return winningScore * -5;
+        }
+        if(node.choice.includes("K") && !node.humanHand.includes("S"))
+        {
+            return winningScore;
+        }
+        if(node.choice == "C" && !node.computerHand.includes("K"))
+        {
+            return winningScore;
+        }
+        if(node.choice == "C" && !node.humanHand.includes("S"))
+        {
+            return winningScore;
+        }
+    }
+    if(maximizingPlayer){
+        var value = Number.NEGATIVE_INFINITY;
+        var result = 0;
+        node.children.forEach(child => {
+            result = emperorMinMax(child, false);
+            if(node.computerHand.length == startingComputerHand.length)
+            {
+                finalChoiceNodes[child.choice] = result;
+            }
+            if(result > value)
+            {
+                value = result;
+            }
+        });
+        return value;
+    }else{
+        var result = 0;
+        node.children.forEach(child => {
+            var prob = child.probability * emperorMinMax(child, true); 
+            result += prob; 
+        });
+        return result;
+    }
+}
+
 function checkTerminalNodeEmperor(node){
-    if(node.choice == "S")
-    {
-        return true;
-    }
-    if(!node.computerHand.includes("K") && node.choice == "C")
-    {
-        return true;
-    }
-    if(node.choice == "S" && node.computerHand.includes("K"))
-    {
-        return true;
-    }
-    if(node.humanHand.length == 1 && node.computerHand.length == 1)
+    if(node.children.length == 0)
     {
         return true;
     }
@@ -189,20 +294,7 @@ function checkTerminalNodeEmperor(node){
 }
 
 function checkTerminalNodeSlave(node){
-    if(node.choice == "K")
-    {
-        return true;
-    }
-    if(!node.computerHand.includes("S") && node.choice == "C")
-    {
-        return true;
-    }
-    if(node.choice == "K" && node.computerHand.includes("S"))
-    {
-        return true;
-    }
-    if(node.humanHand.length == 1 && node.computerHand.length == 1)
-    {
+    if(node.children.length == 0){
         return true;
     }
     return false;
@@ -212,16 +304,17 @@ function addCommas(){
     var val = document.getElementById("bet-amount").value.replace(",","").replace(/[A-Za-z!@#$%^&*()]/g, '');
     val = val.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     document.getElementById("bet-amount").value = val;
-    console.log("VALUE: " + val);
 }
 
 class Node {
-    constructor(computerHand, humanHand, choice, player) {
+    constructor(computerHand, humanHand, choice, player, level, parent) {
         this.computerHand = computerHand;
         this.probability = 0;
         this.humanHand = humanHand;
         this.choice = choice;
         this.player = player;
+        this.level = level;
+        this.parent = parent;
         if(this.player.includes("Computer"))
         {
             if(this.choice == "K")
@@ -269,7 +362,7 @@ class Node {
         return freq;
     }
     buildChildren(){
-        if(this.humanHand.length == 1 && this.computerHand.length == 1)
+        if(this.humanHand.length == 0 && this.computerHand.length == 0)
         {
             return;
         }
@@ -277,28 +370,36 @@ class Node {
         {
             if(this.humanHand.includes("S"))
             {
-                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "S", "Human"));
+                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "S", "Human", this.level + 1, this));
             }
             if(this.humanHand.includes("K"))
             {
-                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "K", "Human"));
+                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "K", "Human", this.level + 1, this));
             }
             if(this.humanHand.includes("C"))
             {
-                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "C", "Human"));
+                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "C", "Human", this.level + 1, this));
             }
         }else{
+            if(this.parent != null && !this.parent.computerHand.includes("K") && !this.parent.computerHand.includes("S"))
+            {
+                return;
+            }
+            if(this.choice == "S" || this.choice == "K")
+            {
+                return;
+            }
             if(this.computerHand.includes("K"))
             {
-                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "K", "Computer"));
+                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "K", "Computer", this.level + 1, this));
             }
             if(this.computerHand.includes("S"))
             {
-                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "S", "Computer"));
+                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "S", "Computer", this.level + 1, this));
             }
             if(this.computerHand.includes("C"))
             {
-                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "C", "Computer"));
+                this.children.push(new Node(this.computerHand.slice(0), this.humanHand.slice(0), "C", "Computer", this.level + 1, this));
             }
         }
     }
